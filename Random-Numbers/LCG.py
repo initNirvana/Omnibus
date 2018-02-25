f982292a66a15c4c8e55dd4160f379b85fe9c1bd
initNirvana <kimqqyun@gmail.com>
2018-02-25 23:34:44 +0900

    LCG for [0,1]

--- Random-Numbers/LCG.py
+++ Random-Numbers/LCG.py
@@ -5,8 +5,15 @@ def LCG(k, c, m, seed):
     while True:
         yield x
         x = (k * x + c) % m
-    
-lcg = LCG(19, 51, 100, 25)
+
+def LCG2(r, x, seed):
+    x = seed
+    while True:
+        yield x
+        x = r * x * (1 - x)
+
+#lcg = LCG(19, 51, 100, 25)
+lcg = LCG2(4, 0.5, 0.8)
 
 for x in lcg:
     print (x)
